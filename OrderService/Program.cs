f1c0822feb580010e451eabfa10f6e040f9fbd96
fzf003 <280086260@qq.com>
2016-04-08 19:03:38 +0800

    no message

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -12,30 +12,20 @@ using TinyMessenger;
 
 namespace OrderService
 {
-    public class TestMessage:TinyMessageBase
-    {
-        public TestMessage() : base(null) { }
-        public string Name { get; set; }
-
-      
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            //using (WebApp.Start("http://127.0.0.1:12345"))
-            //{
-            //   new TinyMessengerHub()
-            //    Console.WriteLine("API服务已启动...");
-            //    Console.ReadKey();
-            //}
+            using (WebApp.Start("http://127.0.0.1:12345"))
+            {
+               
+                Console.WriteLine("API服务已启动...");
+                Console.ReadKey();
+            }
 
-            var hub= new TinyMessengerHub();
-            hub.Subscribe<TestMessage>(p => { Console.WriteLine(p.Name); });
-
-
-            hub.PublishAsync<TestMessage>(new TestMessage() { Name="dssd" });
-            Console.ReadKey();
+            
+           
         }
     }
 }
